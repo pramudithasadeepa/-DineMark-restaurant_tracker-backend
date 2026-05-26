92bc1eb0d95d2b668578329935c2021f39382204
pramudithasadeepa <mlpramuditha1@example.com>
2026-05-26 13:11:15 +0530

    fixed small issues

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ app.use(cors({
 }));
 app.use(express.json());
 
-// Routes
+
 app.use('/api/auth', authRoutes);
 app.use('/api/restaurants', restaurantRoutes);
 app.use('/api/dashboard', dashboardRoutes);
